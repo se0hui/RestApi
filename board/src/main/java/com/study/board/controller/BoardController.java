ee250819a0aa5840851fc3c748748d97893418e5
서희 이 <leesh6541@gmail.com>
2024-06-22 16:30:10 +0900

    first commit

---
+++ board/src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,75 @@
+package com.study.board.controller;
+
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/write") //localhost:8080/board/write
+    public String boardWriteFrom(){
+
+        return "boardwrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board, Model model) {
+
+        boardService.write(board);
+
+        model.addAttribute("message", "글 작성이 완료되었습니다.");
+        model.addAttribute("searchUrl", "/board/list");
+
+
+        return "message";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model, @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+
+        Page<Board> list = boardService.boardList(pageable);
+
+        int nowPage = list.getPageable().getPageNumber() + 1;
+        int startPage = Math.max(nowPage-4,1);
+        int endPage = Math.min(nowPage+5, list.getTotalPages());
+
+
+        model.addAttribute("list", list);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
+        return "boardlist";
+    }
+
+    @GetMapping("/board/view")
+    public String boardView(Model model, @RequestParam(name="id") Integer id) {
+
+        model.addAttribute("board",boardService.boardView(id));
+        return "boardview";
+    }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(@RequestParam(name = "id") Integer id){
+
+        boardService.boardDelete(id);
+
+        return "redirect:/board/list";
+    }
+
+
+
+
+}
